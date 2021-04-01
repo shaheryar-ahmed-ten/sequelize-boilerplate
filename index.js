e793257ef8fae58e0ddc709f3f24eb51f75fd47e
shaheryar_15 <shaheryar@codevative.com>
2021-04-01 11:04:01 +0500

    a=1

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const app = require("./server/app");
 
 const PORT = process.env.PORT || 7000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const a = 1
