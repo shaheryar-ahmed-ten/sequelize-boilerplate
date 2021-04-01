7a11606c58e8c42bdc1922a87ace6763d67fef52
shaheryar_15 <shaheryar@codevative.com>
2021-04-01 11:06:44 +0500

    c

--- index.js
+++ index.js
@@ -4,7 +4,4 @@ const PORT = process.env.PORT || 7000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 const a = 1
-const b = 'b'
-const var2 = '2'
-const temp = "temp"
-const temp2 = "temp2"
+const c = 'c'
